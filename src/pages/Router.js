9903d24ac7999dbbc8dc99f1a937c828df4a93cf
vreality64 <sh.park@tosspayments.com>
2022-06-03 18:12:32 +0900

    fix: Router 페이지 스타일 변경

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { Layout } from "../components/Layout/Layout";
 import { useMock } from '../hooks/useMock';
+import styles from './List.module.css'
 
 export function Router() {
   const router = useRouter();
@@ -10,20 +11,22 @@ export function Router() {
     <Layout title="Router Navigation">
       <h1>Router Navigation</h1>
 
-      {mock.map((value, idx) => {
-        return (
-          <div
-            key={`${value}-${idx}`}
-            onClick={() => {
-              router.push({
-                pathname: `/products/${value}`
-              })
-            }}
-          >
-            {value}
-          </div>
-        )
-      })}
+      <div className={styles.list}>
+        {mock.map((value, idx) => {
+          return (
+            <div
+              key={`${value}-${idx}`}
+              onClick={() => {
+                router.push({
+                  pathname: `/products/${value}`
+                })
+              }}
+            >
+              {value}
+            </div>
+          )
+        })}
+      </div>
     </Layout>
   )
 }
